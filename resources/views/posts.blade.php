fc25b654f9bb24221ca43967812957157dc7b171
Luciano <luciano.denis.a@gmail.com>
2023-12-07 00:32:30 -0300

    Init Project

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Post App</title>
+</head>
+<body>
+  <div class="post">
+    <div class="post-img">
+
+    </div>
+    <div class="post-body">
+      <h3>Post Title</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.</p>
+      <a href="#">Read More</a>
+    </div>
+  </div>
+</body>
+</html>
